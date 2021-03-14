b4aa867eee625ee0603099d15f1f992eed1c938a
apple <apple@apples-imac-6.attlocal.net>
2021-03-14 16:15:38 -0400

    overriding

--- src/day24inheritanceoverriding/Runner.java
+++ src/day24inheritanceoverriding/Runner.java
@@ -4,7 +4,15 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
-		Dog dog1 = new Dog();
+		Dog dog1 = new Dog(true);
+		
+		System.out.println(dog1.height);//3
+		System.out.println(dog1.weight);//4
+		System.out.println(dog1.haveBaby);//true
+		System.out.println(dog1.smellWell);//true
+		
+		Audi audi1 = new Audi();
+		audi1.move();
 
 	}
 
